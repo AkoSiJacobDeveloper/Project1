e9826bf56fa04121a9d888cc2749751245f34233
AkoSiJacobDeveloper <pauljacobtocmo96@gmail.com>
2024-09-16 21:16:05 +0800

    Footer is almost done

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -1,41 +1,44 @@
 <template>
-    <footer class="main-container container-fluid">
-        <div class="container container py-3 px-3">
-            <div class="row row p-3">
-                <div class="col-12 col-md-6 main-wrapper d-flex justify-content-center align-items-center">
-                    <div class="d-flex flex-column align-items-center text-white wrapper-container p-2">
-                        <h5 class="heading text-center fw-bold">Account</h5>
-                        <div v-for="(link, index) in links" :key="index" class="m-2">
-                            <p class="account-link">
-                                <a :href="link.href">{{ link.text }}</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 border-start border-1">
-                    <div class="d-flex flex-column align-items-center text-white wrapper-container">
-                        <h5 class="heading text-center fw-bold">Support</h5>
-                        <div v-for="(support, index) in supports" :key="index" class="m-2">
-                            <p class="support-link">
-                                <a :href="support.href">{{ support.text }}</a>
-                            </p>
-                        </div>
-                    </div>
+    <footer class="footer-main-container pt-2">
+        <div class="container-fluid">
+            <div class="social-media-container d-flex justify-content-between flex-wrap p-md-2 mb-1 text-white">
+                <p class="heading fw-bold text-white m-0 mt-1">Get connected with us on social networks: </p>
+                <div class="d-flex gap-2 icon-container">
+                    <i v-for="(icon, index) in icons" :key="index" :class="icon.class"></i>
                 </div>
             </div>
-            <hr>
-            <div class="row d-flex justify-content-center align-items-center">
-                <div class="col-12 col-md-6 p-2 text-white fs-5">
-                    <div class="accounts-links d-flex gap-3 justify-content-start">
-                        <i v-for="(icon, index) in icons" :key="index" :class="icon.class"></i>
-                    </div>
+            <hr class="text-white mt-0">
+            <div class="row d-flex justify-content-center">
+                <div class="col-12 col-md-6 footer-container">
+                    <h6 class="website-heading fw-bold">Website Name</h6>
+                    <hr class="style-line">
+                    <p class="website-name">QuickBite</p>
                 </div>
-                <div class="col-12 col-md-6">
-                    <div class="footer-text d-flex justify-content-end">
-                        <p class="text m-0 text-white">&copy; 2024 QuickBite. All rights reserved.</p>
-                    </div>
+                <div class="col-12 col-md-6 footer-container">
+                    <h6 class="account-heading fw-bold">Account</h6>
+                    <hr class="style-line">
+                    <p v-for="(account, index) in accounts" :key="index" class="account-link">
+                        <a :href="account.href">{{account.text}}</a>
+                    </p>
+                </div>
+                <div class="col-12 col-md-6 footer-container">
+                    <h6 class="support-links fw-bold">Support Links</h6>
+                    <hr class="style-line">
+                    <p v-for="(support, index) in supports" :key="index" class="support-link">
+                        <a :href="support.href">{{support.text}}</a>
+                    </p>
+                </div>
+                <div class="col-12 col-md-6 footer-container">
+                    <h6 class="contacts fw-bold">Contact</h6>
+                    <hr class="style-line">
+                    <p v-for="(contact, index) in contacts" :key="index" class="contacts-link">
+                        <a :href="contact.href">{{contact.text}}</a>
+                    </p>
                 </div>
             </div>
+        </div>
+        <div class="copyright p-2 p-md-3 p-xl-4 text-center">
+            <p class="text m-0 text-white">&copy; 2024 QuickBite. All rights reserved.</p>
         </div>
     </footer>
 </template>
@@ -44,7 +47,14 @@
 export default {
     data() {
         return {
-            links: [
+            icons: [
+                { class: 'bi bi-facebook' },
+                { class: 'bi bi-instagram' },
+                { class: 'bi bi-twitter' },
+                { class: 'bi bi-youtube' }
+            ],
+
+            accounts: [
                 { href: '#Signin', text: 'Sign In' },
                 { href: '#Login', text: 'Login' }
             ],
@@ -57,59 +67,64 @@ export default {
                 { href: '#Privacypolicy', text: 'Privacy Policy' }
             ],
 
-            icons: [
-                { class: 'bi bi-facebook' },
-                { class: 'bi bi-instagram' },
-                { class: 'bi bi-twitter' },
-                { class: 'bi bi-youtube' }
+            contacts: [
+                { href: '#place', text: 'New York, NY 10012, US' },
+                { href: '#email', text: 'quickbite@gmail.com' },
+                { href: '#call', text: '01 234 567 88' },
+                { href: '#landline', text: '01 234 567 89' }
             ]
-        };
+        }
     }
-};
+}
 </script>
 
 <style lang="scss" scoped>
-footer {
-    padding: 20px 0;
+.footer-main-container {
+    box-sizing: border-box;
     background-color:#4E5340;
-    .container {
-        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-        border-radius: 5px;
-        .row {
+    color: #fff;
+    div {
+        .social-media-container {
+            .heading {
+                font-size: 0.7em;
+            }
+        }
+        div {
+            .footer-container{
+                width: 150px;
+
+                .style-line {
+                    width: 20%;
+                    margin-top: 0px;
+                    color: #fff;
+                }
+                a {
+                    color: #fff;
+                    text-decoration: none;
+                } 
+            }
+        }
+    }
+    .copyright {
+        background-color:#393c2f;
+    }
+}
+
+@media (min-width: 320px) {
+    .footer-main-container {
+        div {
             div {
-                .wrapper-container {
-                    .account-link {
-                        a {
-                            text-decoration: none;
-                            color: #fff;
-
-
-                            &:hover {
-                                text-decoration: underline;
-                            }
-                            
-                        }
-                    }
-
-                    .support-link {
-                        font-size: 1rem;
-                        text-align: center;
-
-                        a {
-                            text-decoration: none;
-                            color: #fff;
-
-                            &:hover {
-                                text-decoration: underline;
-                            }
-                        }
+                .footer-container {
+                    a {
+                        font-size: 0.7em;
                     }
                 }
             }
         }
-        hr {
-            border: 1px solid #fff;
-            margin-top: 0px;
+    }
+    .copyright {
+        .text {
+            font-size: 0.7em;
         }
     }
 }
